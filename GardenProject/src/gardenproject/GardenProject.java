6bd67c9e4049ef4c84bbfbd91a89f313affe1e8d
Maxwellfarray <136000185+Maxwellfarray@users.noreply.github.com>
2023-11-15 10:34:38 -0500

    Update GardenProject.java

--- GardenProject/src/gardenproject/GardenProject.java
+++ GardenProject/src/gardenproject/GardenProject.java
@@ -17,7 +17,7 @@ public class GardenProject
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+       System.out.println("egg");
     }
     
 }
